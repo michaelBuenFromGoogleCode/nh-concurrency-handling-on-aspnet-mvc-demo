ca75df4670bd8e9246b36b728f3add9423b64c46
i.enable.much@gmail.com <i.enable.much@gmail.com@cf6cd63a-be8e-6c32-d890-205cbf29cf07>
2011-07-21 03:31:44 +0000

    Add client-side IValidatableObject capability

--- NhConcurrencyOnAspNetMvc/NhConcurrencyOnAspNetMvc/Controllers/SongController.cs
+++ NhConcurrencyOnAspNetMvc/NhConcurrencyOnAspNetMvc/Controllers/SongController.cs
@@ -8,6 +8,8 @@ using NHibernate.Linq;
 using NhConcurrencyOnAspNetMvc.Models;
 using NHibernate;
 
+using Ienablemuch;
+
 namespace NhConcurrencyOnAspNetMvc.Controllers
 {
     public class SongController : Controller
@@ -33,43 +35,99 @@ namespace NhConcurrencyOnAspNetMvc.Controllers
             }
         }
 
+        
+        [HttpPost]
+        public JsonResult SaveViaAjax(Song song)
+        {
+            SaveCommon(song);
+
+            // you can use the following commented code
+            
+            /* whitebox
+             
+            var v = from m in ModelState.AsEnumerable()
+                    from e in m.Value.Errors
+                    select new { m.Key, e.ErrorMessage };
+            
+            
+            return Json(
+                new 
+                { 
+                    ModelState = 
+                        new 
+                        { 
+                            IsValid = ModelState.IsValid, 
+                            PropertyErrors = v.Where(x => !string.IsNullOrEmpty(x.Key)),
+                            ModelErrors = v.Where(x => string.IsNullOrEmpty(x.Key))
+                        },
+                    SongId = song.SongId,
+                    Version = Convert.ToBase64String(song.Version ?? new byte[]{} )
+                });*/
+
+
+            
+            // alternatively, you can use an extension method, less error-prone
+
+            return Json(
+                new 
+                { 
+                    ModelState = ModelState.ToJsonValidation(),
+                    SongId = song.SongId,
+                    Version = Convert.ToBase64String(song.Version ?? new byte[]{} )
+                });
+
+
+        }
+
 
         [HttpPost]
         public ActionResult Save(Song song)
+        {
+            SaveCommon(song);
+
+            return View("Input", song);
+        }
+
+        private void SaveCommon(Song song)
         {
             using (var s = Mapper.GetSessionFactory().OpenSession())
             using (var tx = s.BeginTransaction())
             {
                 try
                 {
-                    s.SaveOrUpdate(song);
+                    if (ModelState.IsValid)
+                    {
+                        s.SaveOrUpdate(song);
 
-                    tx.Commit();
+                        tx.Commit();
 
-
-                    ModelState.Remove("SongId");
-                    // no need to issue ModelState.Remove on Version property; with byte array, ASP.NET MVC always get its value from the model, not from ModelState
-                    // ModelState.Remove("Version") 
+                        ModelState.Remove("SongId");
+                        // no need to issue ModelState.Remove on Version property; with byte array, ASP.NET MVC always get its value from the model, not from ModelState
+                        // ModelState.Remove("Version") 
+                    }
 
                 }
-                catch(StaleObjectStateException)
-                {                       
+                catch (StaleObjectStateException)
+                {                    
                     s.Evict(song);
                     var dbValues = s.Get<Song>(song.SongId);
                     var userValues = song;
-                   
-                    
-                    if (dbValues == null) 
-                        throw new Exception("This record you are attempting to save is already deleted by other user");
-                   
+
+
+                    if (dbValues == null)
+                    {
+                        ModelState.AddModelError(string.Empty, "This record you are attempting to save is already deleted by other user");
+                        return;
+                    }
+
                     if (dbValues.SongName != userValues.SongName)
                         ModelState.AddModelError("SongName", "Current value made by other user: " + dbValues.SongName);
 
-                    
+
                     if (dbValues.AlbumName != userValues.AlbumName)
                         ModelState.AddModelError("AlbumName", "Current value made by other user: " + dbValues.AlbumName);
 
-                    ModelState.AddModelError(string.Empty, 
+                    ModelState.AddModelError(string.Empty,
                         "The record you attempted to edit was already modified by another user since you last loaded it. Open the latest changes on this record");
 
                 }
@@ -78,8 +136,6 @@ namespace NhConcurrencyOnAspNetMvc.Controllers
                     ModelState.AddModelError(string.Empty, ex.Message);
                 }
 
-
-                return View("Input", song);
             }
         }
 
--- NhConcurrencyOnAspNetMvc/NhConcurrencyOnAspNetMvc/Models/Song.cs
+++ NhConcurrencyOnAspNetMvc/NhConcurrencyOnAspNetMvc/Models/Song.cs
@@ -2,15 +2,49 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace NhConcurrencyOnAspNetMvc.Models
 {
-    public class Song
+    public class Song : IValidatableObject
     {
         public virtual int SongId { get; set; }
-        public virtual string SongName { get; set; }
-        public virtual string AlbumName { get; set; }
+        [Required] public virtual string SongName { get; set; }
+        [Required] public virtual string AlbumName { get; set; }
 
         public virtual byte[] Version { get; set; }
+
+        public virtual IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+
+            // model-level validation
+            if (SongName == "blah" && AlbumName == "meh")
+            {
+                yield return new ValidationResult("Combined song name and album name cannot be blah and meh");
+                yield return new ValidationResult("*", new[] { "SongName", "AlbumName" });                
+            }
+            else
+            {
+
+                // model-level validation
+                if (SongName[0] != AlbumName[0])
+                {
+                    yield return new ValidationResult("Song name and album name must start with same letter");
+                    yield return new ValidationResult("*", new[] { "AlbumName", "SongName" });                    
+                }
+
+                // inline style
+                if (!SongName.ToUpper().Contains("LOVE"))
+                    yield return new ValidationResult("Song name must have a word love", new[] { "SongName" });
+
+                // property, but asterisk style
+                if (!AlbumName.ToUpper().Contains("GREAT"))
+                {
+                    yield return new ValidationResult("Album name must have a word great");
+                    yield return new ValidationResult("*", new[] { "AlbumName" });                    
+                }
+            }
+            
+        }
     }
 }
