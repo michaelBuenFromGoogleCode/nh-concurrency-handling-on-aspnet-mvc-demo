80999ebf8771a036b2a09182aa9c47f84356091a
i.enable.much@gmail.com <i.enable.much@gmail.com@cf6cd63a-be8e-6c32-d890-205cbf29cf07>
2011-07-21 03:31:44 +0000

    Add client-side IValidatableObject capability

---
+++ trunk/NhConcurrencyOnAspNetMvc/Ienablemuch/ValidationExtension.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+
+namespace Ienablemuch
+{
+
+    class Test
+    {
+        public static void Main()
+        {
+            Type t = typeof(Test);
+            string s = t.Assembly.FullName.ToString();
+            Console.WriteLine("The fully qualified assembly name " +
+                "containing the specified class is {0}.", s);
+
+            Console.ReadLine();
+        }
+    }
+
+    public static class ValidationExtension
+    {
+
+        public static object ToJsonValidation(this ModelStateDictionary modelState)
+        {
+            var v = from m in modelState.AsEnumerable()
+                    from e in m.Value.Errors
+                    select new { m.Key, e.ErrorMessage };
+
+            return new
+            {
+                IsValid = modelState.IsValid,
+                PropertyErrors = v.Where(x => !string.IsNullOrEmpty(x.Key)),
+                ModelErrors = v.Where(x => string.IsNullOrEmpty(x.Key))
+            };
+        }
+
+
+        public static MvcHtmlString JsAccessibleValidationSummary(this HtmlHelper htmlHelper)
+        {
+            return htmlHelper.ValidationSummary();
+        }
+
+
+        public static MvcHtmlString JsAccessibleValidationSummary(this HtmlHelper htmlHelper, bool excludePropertyErrors)
+        {
+            if (!htmlHelper.ViewData.ModelState.IsValid || !excludePropertyErrors)
+                return htmlHelper.ValidationSummary(excludePropertyErrors);
+            else
+            {
+                return htmlHelper.BuildValidationSummary(null, null);
+            }
+        }
+
+        public static MvcHtmlString JsAccessibleValidationSummary(this HtmlHelper htmlHelper, string message)
+        {
+            return htmlHelper.ValidationSummary(message);
+        }
+
+        public static MvcHtmlString JsAccessibleValidationSummary(this HtmlHelper htmlHelper, bool excludePropertyErrors, string message)
+        {
+            if (!htmlHelper.ViewData.ModelState.IsValid || !excludePropertyErrors)
+                return htmlHelper.ValidationSummary(excludePropertyErrors, message);
+            else
+                return htmlHelper.BuildValidationSummary(message, (IDictionary<string, object>)null);
+
+        }
+
+
+        public static MvcHtmlString JsAccessibleValidationSummary(this HtmlHelper htmlHelper, string message, object htmlAttributes)
+        {
+            return htmlHelper.ValidationSummary(message, htmlAttributes);
+        }
+
+
+        public static MvcHtmlString JsAccessibleValidationSummary(this HtmlHelper htmlHelper, bool excludePropertyErrors, string message, object htmlAttributes)
+        {
+            if (!htmlHelper.ViewData.ModelState.IsValid || !excludePropertyErrors)
+                return htmlHelper.ValidationSummary(excludePropertyErrors, message, htmlAttributes);
+            else
+                return htmlHelper.BuildValidationSummary(message, HtmlHelper.AnonymousObjectToHtmlAttributes(htmlAttributes));
+        }
+
+        public static MvcHtmlString JsAccessibleValidationSummary(this HtmlHelper htmlHelper, string message, IDictionary<string, object> htmlAttributes)
+        {
+            return htmlHelper.ValidationSummary(message, htmlAttributes);
+        }
+
+        public static MvcHtmlString JsAccessibleValidationSummary(this HtmlHelper htmlHelper, bool excludePropertyErrors, string message, IDictionary<string, object> htmlAttributes)
+        {
+            if (!htmlHelper.ViewData.ModelState.IsValid || !excludePropertyErrors)
+                return htmlHelper.ValidationSummary(excludePropertyErrors, message, htmlAttributes);
+            else
+                return htmlHelper.BuildValidationSummary(message, htmlAttributes);
+        }
+
+
+        private static MvcHtmlString BuildValidationSummary(this HtmlHelper htmlHelper, string message, IDictionary<string, object> htmlAttributes)
+        {
+            // excludePropertyErrors is always true here
+
+            string messageSpan;
+            if (!String.IsNullOrEmpty(message))
+            {
+                TagBuilder spanTag = new TagBuilder("span");
+                spanTag.SetInnerText(message);
+                messageSpan = spanTag.ToString(TagRenderMode.Normal) + Environment.NewLine;
+            }
+            else
+            {
+                messageSpan = null;
+            }
+
+            TagBuilder divBuilder = new TagBuilder("div");
+
+            divBuilder.AddCssClass(HtmlHelper.ValidationSummaryValidCssClassName);
+            divBuilder.InnerHtml = messageSpan + "<ul></ul>";
+
+
+            // I use false, so jQuery won't be confused putting property validation on summary
+            divBuilder.MergeAttribute("data-valmsg-summary", "false");
+
+            divBuilder.MergeAttributes(htmlAttributes);
+
+
+            return divBuilder.ToMvcHtmlString(TagRenderMode.Normal);
+        }
+
+        // copied from ASP.NET MVC. so many internals in ASP.NET MVC :-)
+        private static MvcHtmlString ToMvcHtmlString(this TagBuilder tagBuilder, TagRenderMode renderMode)
+        {
+            System.Diagnostics.Debug.Assert(tagBuilder != null);
+            return new MvcHtmlString(tagBuilder.ToString(renderMode));
+        }
+
+    }
+}
---
+++ trunk/NhConcurrencyOnAspNetMvc/NhConcurrencyOnAspNetMvc/Scripts/IEnableMuch/ienablemuch.js
@@ -0,0 +1,77 @@
+﻿
+(function ($) {
+
+    $.fn.modelValidation = function (data) {
+
+        var form = $(this);
+
+        if (form.size() > 1) {
+            alert('There are more than one form in this page. Contact the dev to indicate the specific form that needed be validated');
+            return;
+        }
+
+
+
+        if (!data.ModelState.IsValid) {
+            showInvalid(data, form);
+        }
+        else
+            clearInvalid();
+
+        return data.ModelState.IsValid;
+
+    }
+
+
+
+    function clearInvalid(form) {
+        $('div[data-valmsg-summary]', form).removeClass().addClass('validation-summary-valid');
+    }
+
+    function showInvalid(data, form) {
+
+        valSum = $('div[data-valmsg-summary]', form);
+
+
+        if (valSum.attr('data-valmsg-summary') == undefined)
+            valSum = null;
+
+
+
+        var errorList = null;
+        if (valSum) {
+            valSum.removeClass().addClass('validation-summary-errors');
+            errorList = $('> ul', valSum);
+            errorList.html('');
+        }
+
+
+        $.each(data.ModelState.PropertyErrors, function () {
+
+            if (valSum && valSum.attr('data-valmsg-summary') == 'true') {
+                errorList.append($('<li />').text(this.ErrorMessage));
+            }
+
+
+            var propVal = $('span[data-valmsg-for=' + this.Key + ']', form);
+            propVal.removeClass().addClass('field-validation-error').text(this.ErrorMessage);
+
+            var prop = $('input[name=' + this.Key + ']', form);
+            prop.addClass('input-validation-error');
+        });
+
+
+
+        if (valSum) {
+            $.each(data.ModelState.ModelErrors, function () {
+                errorList.append($('<li />').text(this.ErrorMessage));
+            });
+        }
+
+    }//showInvalid
+
+
+})(jQuery);
+
+
+
